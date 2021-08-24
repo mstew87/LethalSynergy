f67b037517fa145eb02c036dd599b7d943bbe520
MikeStew <mike.a.stewart87@gmail.com>
2021-08-24 11:26:43 -0700

    updated dropdown menu, switched to dark theme, fixed bootstrap js links

--- LethalSynergyApp/urls.py
+++ LethalSynergyApp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path ('', views.index, name='index'),
     path ('about', views.about),
     path ('supported', views.supported),
+    path ('ffxiv', views.ffxiv),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
