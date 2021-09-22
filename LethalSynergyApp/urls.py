9ad95579792faa5354719b85834fc141b7816980
MikeStew <mike.a.stewart87@gmail.com>
2021-09-21 21:17:19 -0700

    Removed views and urls for supported template. Was replaced with individual game templates.

--- LethalSynergyApp/urls.py
+++ LethalSynergyApp/urls.py
@@ -5,7 +5,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path ('', views.index, name='index'),
     path ('about', views.about),
-    path ('supported', views.supported),
     path ('ffxiv', views.ffxiv),
     path ('lost_ark', views.lost_ark),
     path ('eso', views.eso),
--- LethalSynergyApp/views.py
+++ LethalSynergyApp/views.py
@@ -6,9 +6,6 @@ def index(request):
 def about(request):
   return render(request,'about.html')
 
-def supported(request):
-  return render(request, 'supported.html')
-
 def ffxiv(request):
   return render(request, 'ffxiv.html')
 
